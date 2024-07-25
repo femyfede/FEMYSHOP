f3dfc32d90bfe14d46cfb071da10611e36322715
femyfede <156244790+femyfede@users.noreply.github.com>
2024-07-25 03:02:25 -0700

    initial commit

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
-from .views import home
 
 urlpatterns = [
     path('',views.registration, name='regdjango'),
-    path('logindjango/',views.login,name='logindjango'),
+    path('login/',views.login,name='logindjango'),
      path('home/', views.home, name='home'),
 ]
